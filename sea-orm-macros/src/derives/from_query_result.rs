83a6b5f14a50514ebe9ea0d23916ec942601ba0a
arucil <xplzjwz@gmail.com>
2023-03-13 11:02:04 +0800

    feat: support column_name for FromQueryResult

--- sea-orm-macros/src/derives/from_query_result.rs
+++ sea-orm-macros/src/derives/from_query_result.rs
@@ -1,6 +1,6 @@
 use proc_macro2::{Ident, TokenStream};
 use quote::{format_ident, quote, quote_spanned};
-use syn::{ext::IdentExt, Data, DataStruct, Fields, Meta};
+use syn::{ext::IdentExt, Data, DataStruct, Fields, Lit, Meta};
 
 use crate::util::process_field_attrs;
 
@@ -18,35 +18,55 @@ pub fn expand_derive_from_query_result(ident: Ident, data: Data) -> syn::Result<
         }
     };
 
-    let fields = fields.into_iter().map(|field| {
-        let name = format_ident!("{}", field.ident.as_ref().unwrap().to_string());
-        let name_str = name.unraw().to_string();
-        let mut has_default = false;
-        let _: Result<(), ()> = process_field_attrs(&field, |meta| {
-            if let Meta::Path(path) = meta {
-                if let Some(name) = path.get_ident() {
-                    if name == "default" {
-                        has_default = true;
-                    }
-                }
-            }
-            Ok(())
-        });
-        if has_default {
-            quote! {
-                #name: row.try_get(pre, #name_str)
-                    .or_else(|err| {
-                        if err.is_column_not_found_error() {
-                            Ok(Default::default())
-                        } else {
-                            Err(err)
+    let fields = fields
+        .into_iter()
+        .map(|field| {
+            let name = format_ident!("{}", field.ident.as_ref().unwrap().to_string());
+            let mut name_str = name.unraw().to_string();
+            let mut has_default = false;
+            process_field_attrs(&field, |meta| {
+                match meta {
+                    Meta::Path(path) => {
+                        if let Some(name) = path.get_ident() {
+                            if name == "default" {
+                                has_default = true;
+                            }
                         }
-                    })?
+                    }
+                    Meta::NameValue(nv) => {
+                        if let Some(name) = nv.path.get_ident() {
+                            if name == "column_name" {
+                                if let Lit::Str(name) = &nv.lit {
+                                    name_str = name.value();
+                                } else {
+                                    return Err(syn::Error::new(
+                                        nv.lit.span(),
+                                        format!("Invalid column_name {:?}", nv.lit),
+                                    ));
+                                }
+                            }
+                        }
+                    }
+                    _ => {}
+                }
+                Ok(())
+            })?;
+            if has_default {
+                Ok(quote! {
+                    #name: row.try_get(pre, #name_str)
+                        .or_else(|err| {
+                            if err.is_column_not_found_error() {
+                                Ok(Default::default())
+                            } else {
+                                Err(err)
+                            }
+                        })?
+                })
+            } else {
+                Ok(quote! { #name: row.try_get(pre, #name_str)? })
             }
-        } else {
-            quote! { #name: row.try_get(pre, #name_str)? }
-        }
-    });
+        })
+        .collect::<syn::Result<Vec<_>>>()?;
 
     Ok(quote!(
         #[automatically_derived]
